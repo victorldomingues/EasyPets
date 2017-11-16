edace9c0b3effa23ae1008edc07bea14a7eb31f4
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2017-11-16 01:01:08 -0200

    create services, adoption and pets routes

---
+++ app/Http/Controllers/PetsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PetsController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('adoption.pets');
+    }
+}
